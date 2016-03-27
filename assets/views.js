b0cf40750f3cc9a7a1131ca34e08703cb00027c0
Leonid Fenko <fenok2112@gmail.com>
2016-03-27 20:52:03 +0300

    Restyling

--- assets/views.js
+++ assets/views.js
@@ -1,31 +1,43 @@
-function formPager(current, showing, total)
+var total   = 20;
+var showing = 7;
+
+function formPager( current, showing, total )
 {
-	var pagerDiv = document.getElementById("pager");
+	var pagerDiv       = document.getElementById( "pager" );
 	pagerDiv.innerHTML = '';
-	var numbers = getNumbersArray(current, showing, total);
+	var numbers        = getNumbersArray( current, showing, total );
 	var ref;
-	for(var index = 0; index < numbers.length; ++index)
+	for ( var index = 0; index < numbers.length; ++index )
 	{
-		ref = document.createElement('a');
-		ref.href = "#" + numbers[index];
-		ref.innerText = numbers[index];
-		pagerDiv.appendChild(ref);
-		if ((index === 0 && numbers[index+1] !== 2) || (index === numbers.length - 2 && numbers[index] !== total - 1))
+		ref           = document.createElement( 'a' );
+		ref.href      = "#" + numbers[ index ];
+		ref.innerText = numbers[ index ];
+		if ( numbers[ index ] === current )
 		{
-			ref = document.createElement('span');
+			ref.className = 'selected';
+		}
+		pagerDiv.appendChild( ref );
+		if ( (index === 0 && numbers[ index + 1 ] !== 2) || (index === numbers.length - 2 && numbers[ index ] !== total - 1) )
+		{
+			ref           = document.createElement( 'span' );
 			ref.innerText = '...';
-			pagerDiv.appendChild(ref);
+			pagerDiv.appendChild( ref );
 		}
 	}
 }
 
-window.addEventListener('hashchange', function()
+window.addEventListener( 'hashchange', function()
 {
-	formPager(window.location.hash.substr(1),7,20);
-});
+	formPager( parseInt(window.location.hash.substr( 1 )), showing, total );
+} );
 
-window.addEventListener('load', function()
+window.addEventListener( 'load', function()
 {
-	window.location.hash = '#1';
-	formPager(window.location.hash.substr(1),7,20);
-});
+	var hashValue = window.location.hash.substr( 1 );
+	if ( !Number.isInteger( hashValue ) || 1 >= hashValue || hashValue >= total )
+	{
+		window.location.hash = '#1';
+		hashValue            = '1';
+	}
+	formPager( parseInt(hashValue), showing, total );
+} );
